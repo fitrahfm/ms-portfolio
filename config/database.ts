80a96c1bfdd3d8d2762ca44a7ae9269afb8edc28
Fitrah Munir <fitrahfm@Fitrahs-MacBook-Pro.local>
2023-03-17 21:58:47 +0800

    add summary & location

--- config/database.ts
+++ config/database.ts
@@ -2,12 +2,12 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ep-gentle-meadow-053963.ap-southeast-1.aws.neon.tech'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'portfolio'),
-      user: env('DATABASE_USERNAME', 'fitrhmnr+hasura1'),
-      password: env('DATABASE_PASSWORD', 'lpbBKNWJuO78'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL'),
     },
   },
 });
